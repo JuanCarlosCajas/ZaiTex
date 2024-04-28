245a450d7186f7a23197ad56e64fe0a4477724ae
Rakona <rakona2310@gmail.com>
2024-04-27 20:03:31 -0500

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Header } from './Header'
-
+import { Body } from './Body'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Body/>
     </>
   )
 }
---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+import './Body.css'
+
+export function Body(){
+    
+    return(
+        <>
+            <img src="img/body.png" alt="body" className='cuerpo-contenedor'/>
+            <div className='Z-logo'></div>
+            <img src="img/persona_1.png" className='persona-1' width={"700px"} alt="persona" />
+            <div className='beneficios-contenedor'>
+
+            </div>
+        </>
+    )
+}
